5b28e8ca6c4987d553fc6ef1f7b664ce0040c22f
Kazuma275 <sergioramosalarcon@gmail.com>
2025-06-19 17:02:48 +0200

    fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
-import { ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-variants"
+import { ClassValue } from "clsx"
+import clsx from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(...inputs))
